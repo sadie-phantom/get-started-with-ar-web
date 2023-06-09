f8ef700e9c0be4922ed307c2f7cd0e9077992402
Sadie <135024408+sadie-phantom@users.noreply.github.com>
2023-06-09 11:55:58 +0100

    initial red box

--- src/scene.ts
+++ src/scene.ts
@@ -15,5 +15,31 @@ import {
 } from "three";
 
 export function createScene(renderer: WebGLRenderer) {
-  // TODO: Create a scene and build a WebXR app!
+  const scene = new Scene()
+
+  const camera = new PerspectiveCamera(
+    70,
+    window.innerWidth / window.innerHeight,
+    0.02,
+    20,
+  )
+
+  const boxGeometry = new BoxBufferGeometry(1, 1, 1);
+  const boxMaterial = new MeshBasicMaterial({ color: 0xff0000 });
+  const box = new Mesh(boxGeometry, boxMaterial);
+  box.position.z = -3;
+
+  scene.add(box);
+
+  const renderLoop = (timestamp: number, frame?: XRFrame) => {
+    // Rotate box
+    box.rotation.y += 0.01;
+    box.rotation.x += 0.01;
+
+    if (renderer.xr.isPresenting) {
+      renderer.render(scene, camera);
+    }
+  }
+  
+  renderer.setAnimationLoop(renderLoop);
 }
